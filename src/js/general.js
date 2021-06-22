c7c992f4384befaff7fce85e06282fc02bb52311
Vki-frodo <victor.vki.dasilva@gmail.com>
2021-06-21 22:41:42 -0300

    upload para avaliacao

---
+++ src/js/general.js
@@ -0,0 +1,8 @@
+function sabado(witch) {
+    for (let index = 1; index < 9; index++) {
+        document.getElementById(index).style.display = "none";
+    }
+
+    document.getElementById(witch).style.display = "flex";
+    console.log("batman")
+}
