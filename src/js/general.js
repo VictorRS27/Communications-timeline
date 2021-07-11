c315cbc2d32a21ccb37edf35054d78105132f947
Vki-frodo <victor.vki.dasilva@gmail.com>
2021-07-11 10:33:50 -0300

    changing palette and creating informations content layout

--- src/js/general.js
+++ src/js/general.js
@@ -1,8 +1,12 @@
+Object, onload = function() {
+    document.getElementById(1).style.display = "flex";
+}
+
 function sabado(witch) {
     for (let index = 1; index < 9; index++) {
         document.getElementById(index).style.display = "none";
     }
 
-    document.getElementById(witch).style.display = "flex";
-    console.log("batman")
+    document.getElementById(witch).style.display = "block";
+    console.log("ta em shok? chama o batman")
 }
