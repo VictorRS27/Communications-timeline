f12bb907ed25401985d0e087ddaff47cf7717032
Vki-frodo <victor.vki.dasilva@gmail.com>
2021-07-19 12:24:10 -0300

    limpeza para versão de distribuição

---
+++ js/general.js
@@ -0,0 +1,11 @@
+Object, onload = function() {
+    document.getElementById(6).style.display = "block";
+}
+
+function sabado(witch) {
+    for (let index = 1; index < 9; index++) {
+        document.getElementById(index).style.display = "none";
+    }
+
+    document.getElementById(witch).style.display = "block";
+}
